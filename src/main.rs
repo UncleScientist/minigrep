c1583b840d595bd300ad192018f8a45110d57b96
Uncle Scientist <unclescientist@gmail.com>
2021-04-27 18:57:24 -0400

    handle the Err() return

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::env;
 use std::fs;
 use std::process;
@@ -13,14 +14,19 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+        fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
